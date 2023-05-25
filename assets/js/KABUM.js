58e564ddfce51081c1b6d7747f30fabe33c79c98
Thiago Sampaio <contatosampaiothiago@gmail.com>
2023-05-25 01:00:13 -0300

    validando forms

--- assets/js/KABUM.js
+++ assets/js/KABUM.js
@@ -6,20 +6,31 @@ const p = document.createElement('p');
 back.appendChild(p);
 
 
-
+formValid =()=>{
+    let msg = '';
+    if(cont.value === '') msg += 'Campo não preenchido .\n';
+    if(msg != ''){
+        alert(msg);
+        return false;
+    }
+    return true;
+}
 const startBUM =()=>{
-    gambs.classList.add('d-none')
-    let contador = Number(cont.value);
-    let interval = setInterval(()=>{
-        p.innerHTML+= `${contador} \n`;
-       contador-=1;
-        if(contador < -1){
-            p.innerHTML = '';
-            img.classList.add('d-flex');
-        }
-        if(contador< -4){
-            clearInterval(interval);
-           window.location.reload(true);
-        }
-    },1000);
+    if(formValid()){
+        gambs.classList.add('d-none')
+        let contador = Number(cont.value);
+        let interval = setInterval(()=>{
+            p.innerHTML+= `${contador} \n`;
+           contador-=1;
+            if(contador < -1){
+                p.innerHTML = '';
+                img.classList.add('d-flex');
+            }
+            if(contador< -4){
+                clearInterval(interval);
+               window.location.reload(true);
+            }
+        },1000);
+
+    }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,27 +14,41 @@ const p6 = document.createElement('p');
 
 
 const handleClick = () => {
-    const name = containerName.value;
-    const date = containerDate.value;
-    const telefone = tel.value;
-    const numDate=date.split('-').reverse().join('');
-    p.innerText = `Seu nome é : "${name}."`;                                                              
-    p1.innerText = ` Contém : ${name.length} caracteres.`;                                                    
-    p2.innerText =  ` Seu nome ao contrário é: ${name.split('').reverse().join('')}.  `; 
-    p3.innerText = `A metade de seu nome é: ${name.slice(0,name.length/2)}`;                                    
-    p4.innerText =  ` Sua data de nascimento é: ${date.split('-').reverse().join('-')}.`;                                            
-    p5.innerText = ` A soma dos numeros da sua data de nascimento é: ${numDate.split('').join('+')} = ${soma(numDate.split(''))}.`;
-    p6.innerText = ZoneDDD(telefone.slice(0,2));
-    back.appendChild(p);
-    back.appendChild(p1);
-    back.appendChild(p2);
-    back.appendChild(p3);
-    back.appendChild(p4);
-    back.appendChild(p5);
-    back.appendChild(p6);
+    if(formValid()){
+        const name = containerName.value;
+        const date = containerDate.value;
+        const telefone = tel.value;
+        const numDate=date.split('-').reverse().join('');
+        p.innerText = `Seu nome é : "${name}."`;                                                              
+        p1.innerText = ` Contém : ${name.length} caracteres.`;                                                    
+        p2.innerText =  ` Seu nome ao contrário é: ${name.split('').reverse().join('')}.  `; 
+        p3.innerText = `A metade de seu nome é: ${name.slice(0,name.length/2)}`;                                    
+        p4.innerText =  ` Sua data de nascimento é: ${date.split('-').reverse().join('-')}.`;                                            
+        p5.innerText = ` A soma dos numeros da sua data de nascimento é: ${numDate.split('').join('+')} = ${soma(numDate.split(''))}.`;
+        p6.innerText = ZoneDDD(telefone.slice(0,2));
+        back.appendChild(p);
+        back.appendChild(p1);
+        back.appendChild(p2);
+        back.appendChild(p3);
+        back.appendChild(p4);
+        back.appendChild(p5);
+        back.appendChild(p6);
+        
+    }
     
     
-    
+}
+formValid =()=>{
+    let msg = '';
+    if(containerName.value === '') msg += 'Campo nome não preenchido .\n';
+    if(containerDate.value === '') msg += 'Campo data não preenchido .\n';
+    if(containerDate.value.split("-")[0] < 1900 || containerDate.value.split("-")[0] > 2023) msg += 'Data invalida .\n';
+    if(tel.value=== '') msg += 'Campo telefone não preenchido .\n';
+    if(msg != ''){
+        alert(msg);
+        return false;
+    }
+    return true;
 }
 
 const soma = (data) =>{
